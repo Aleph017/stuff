de51c6b080a96e4cf32c8a5e9e45f2a898d8b8b7
Aleph017 <turdiev1@gmail.com>
2024-11-16 19:09:15 +0300

    Delete python_stuff/06_catbox.py

--- python_stuff/06_catbox.py
+++
@@ -1,6 +0,0 @@
-cat_name = "Гера"
-box_color = "коричневая"
-box_location = "на столе"
-
-print(f"На столе стоит коробка. В ней валяется кошка по имени {cat_name}")
-print(f"Коробка {box_color} и находится {box_location}")
